9b2c438ee24b3606d91994053725a9e887a24689
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-09 15:51:48 +0200

    adding this exercises

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ type Address={
 
 }
 type Pizza={
+    id:number
     name:string
     price:number
     //address?:Address //optional properties
@@ -16,11 +17,11 @@ type Order={
     pizza:Pizza
     status:Status
 }
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu :Pizza[]= [
+    {id:1, name: "Margherita", price: 8 },
+    {id:2, name: "Pepperoni", price: 10 },
+    {id:3, name: "Hawaiian", price: 10 },
+    {id:4, name: "Veggie", price: 9 },
 ]
  let cashInRegister=100
  const orderQueue:Order[]=[]
@@ -51,9 +52,17 @@ const menu = [
      order.status='completed'
      return order
  }
- addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+ function getPizzaDetails(identifier:string|number){
+ if(typeof identifier ==='string'){
+    return menu.find(pizza=>pizza.name.toLowerCase()=== identifier.toLowerCase())
+ }
+ else{
+    return menu.find(pizza=>pizza.id=== identifier)
+ }
+ }
+ addNewPizza({id:5, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({id:6, name: "BBQ Chicken", price: 12 })
+addNewPizza({id:7, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
