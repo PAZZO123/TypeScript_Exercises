a6434dfd669cf2ade206d56a14cda7b8a52e3c90
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-14 17:50:08 +0200

    adding this kata

---
+++ Codewars/6kyu/PyramidArray.ts
@@ -0,0 +1,11 @@
+ function pyramid(n: number) : Array<Array<Number>> {
+  let result :any[]=[]
+  for(let i=1;i<=n;i++){
+    let current:number[]=[]
+    for(let j=1;j<=i;j++){
+      current.push(1)
+    }
+    result.push(current)
+  }
+    return result;
+}
