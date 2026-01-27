084636404279ae18ae6cdaa0be35e971d2abc92d
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-27 11:44:57 +0200

    Adding this to the Github

---
+++ exerciese/Tuples.js
@@ -0,0 +1,5 @@
+var tuple = ['pazzo', 23]; //as const;
+console.log(tuple);
+tuple.push('dream');
+tuple.push(19);
+console.log(tuple);
---
+++ exerciese/Tuples.ts
@@ -0,0 +1,5 @@
+let tuple:[string ,number]=['pazzo',23] //as const;
+console.log(tuple)
+tuple.push('dream')
+tuple.push(19)
+console.log(tuple)
