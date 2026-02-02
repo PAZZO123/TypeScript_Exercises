0ec9014ee19b3b7b7b1257a70c4a44ff8fbbbaab
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-02 14:25:29 +0200

    Adding this

---
+++ Record.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const namedMap = {
+    'name': 12,
+    'Age': 456
+};
+console.log(namedMap);
+//# sourceMappingURL=Record.js.map
---
+++ Record.ts
@@ -0,0 +1,5 @@
+const namedMap:Record<string,number>={
+    'name':12,
+    'Age':456
+}
+ console.log(namedMap)
